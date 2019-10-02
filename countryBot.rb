74c52e9b093ba42406614083dee02825604ad231
Sylvain <sypsilon@hotmail.com>
2019-10-02 20:28:09 +0200

    Make bot to train bot

--- countryBot.rb
+++ countryBot.rb
@@ -3,6 +3,10 @@ require "open-uri"
 require 'json'
 
 token ='794487993:AAHdGN4he41TElP074Z5OdF2s97mzuglUhg'
+re1='.*?'	# Non-greedy match on filler
+re2='(\\d+)'	# Integer Number 1
+re=(re1+re2)
+m=Regexp.new(re,Regexp::IGNORECASE);
 
 bot = TelegramBot.new(token: token)
 
@@ -12,37 +16,18 @@ bot.get_updates(fail_silently: true) do |message|
 
     message.reply do |reply|
       result = ""
-      i = 0
     case command
     when /start/i
-      reply.text = "I know all the Countries!"
-    else
-      begin
-        resString = JSON.parse(URI.parse("https://restcountries.eu/rest/v2/name/#{message.text}").read)
-      rescue StandardError => e
-        reply.text = "The country #{message.text} couldn't be found"
-        reply.send_with(bot)
-        break
-      end
-      resString.map { |e|
-        result = ""
-        e.each do |q,i|
-          unless q == "alpha2Code" || q == "alpha3Code" || q == "latlng" || q == "gini"
-            puts q
-            if i.instance_of?(Array)
-              result += q +":  "+ i.join(",") + "\n\n"
-            elsif i.instance_of?(Integer) || i.instance_of?(Hash) || i.instance_of?(Float)
-              result += q +":  "+ i.to_s + "\n\n"
-            elsif i.nil? || e == 0
-              result += q + "\n\n"
-            else
-              result += q +":  "+ i + "\n\n"
-            end
-          end
-        end
-        reply.text = result
-        reply.send_with(bot)
-      }
+      reply.text = "I am your Trainer"
+    when /p/i      
+      reply.text = m.match(message.text)[1] + " Pushups have benn added";
+      puts m.match(message.text)[1].to_i + 1
+      File.write('/home/sylvain/Documents/countryBot/pushup.txt', File.read('/home/sylvain/Documents/countryBot/pushup.txt').to_i + m.match(message.text)[1].to_i)
+    when /c/i
+      reply.text = m.match(message.text)[1] + " Crunches have benn added"
+      File.write('/home/sylvain/Documents/countryBot/crouch.txt', File.read('/home/sylvain/Documents/countryBot/crouch.txt').to_i + m.match(message.text)[1].to_i)
+    when /stats/i
+      reply.text = File.read('/home/sylvain/Documents/countryBot/crouch.txt') + " Crouches " + File.read('/home/sylvain/Documents/countryBot/pushup.txt') + " Pushups"
     end
     reply.send_with(bot)
   end
